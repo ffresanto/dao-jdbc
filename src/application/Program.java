c5e75a07caa988fa50c20329b996bb1e6730b1b3
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2020-01-25 13:57:18 -0300

    FindById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,16 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "books");
-        Seller seller = new Seller(21, "micael", "micael@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
